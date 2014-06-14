b79d196f12991f651f62c45def97e75cb012a00c
Alvarotaol <ato.cdf@gmail.com>
2014-06-14 10:27:29 -0300

    Comecei a fazer a lógica do jogo

--- src/controle/Grade.java
+++ src/controle/Grade.java
@@ -1,11 +1,14 @@
 package controle;
 
+import logica.Campo;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 
 public class Grade {
 	Rectangle[][] grade;
+	Campo c;
 	public static int x, y, dimx, dimy, dimq;
 	public Grade(int x, int y, int dx, int dy, int dq) {
 		this.x = x;
@@ -19,16 +22,22 @@ public class Grade {
 				grade[i][j] = new Rectangle(x + i*dq, y + j*dq, dq, dq);
 			}
 		}
+		c = new Campo(dx, dy, dx*dy/10);
+		c.novoJogo();
 	}
 	
 	public void desenhar(int mx, int my, Graphics g){
 		for (int i = 0; i < dimx; i++) {
 			for (int j = 0; j < dimy; j++) {
-				if (grade[i][j].contains(mx, my)) {
-					g.setColor(Color.lightGray);
-					g.fill(grade[i][j]);
+				if(c.getEstado(i, j) == 1){
+					g.drawString("" + c.getCampo(i, j), i*dimq + x + dimq / 4, j*dimq + y + dimq / 5);
 				} else {
-					g.draw(grade[i][j]);
+					if (grade[i][j].contains(mx, my)) {
+						g.setColor(Color.lightGray);
+						g.fill(grade[i][j]);
+					} else {
+						g.draw(grade[i][j]);
+					}
 				}
 			}
 		}
---
+++ src/logica/Campo.java
@@ -0,0 +1,88 @@
+package logica;
+
+import java.util.Random;
+
+public class Campo {
+	private int x, y, bombas;
+	private int[][] estado, grade;
+	
+	public Campo(int x, int y, int bombas) {
+		this.x = x;
+		this.y = y;
+		this.bombas = bombas;
+		grade = new int[x][y];
+		estado = new int[x][y];
+	}
+	
+	public int getEstado(int i, int j){
+		return (grade[i][j] == 0)?1:0; //estado[i][j];
+	}
+	
+	public int getCampo(int i, int j){
+		return grade[i][j];
+	}
+	
+	public void novoJogo(){
+		int i = 0;
+		Random gerar = new Random();
+		while(i < bombas){
+			int nx = gerar.nextInt(x), ny = gerar.nextInt(y);
+			while(grade[nx][ny] >= 9){
+				nx = gerar.nextInt(x);
+				ny = gerar.nextInt(y);
+			}
+			grade[nx][ny] = 9;
+			somar(nx, ny);
+			i++;
+		}
+		for(i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				if (grade[i][j] > 9) {
+					grade[i][j] = 9;
+				}
+			}
+		}
+	}
+	
+	private void somar(int i, int j){
+		if(i != 0){
+			if(j != 0){
+				grade[i-1][j-1]++;
+			}
+			if(j < y - 1){
+				grade[i-1][j+1]++;
+			}
+			grade[i-1][j]++;
+		}
+		if(j != 0){
+			grade[i][j-1]++;
+		}
+		if(i != y - 1){
+			if(j != 0){
+				grade[i+1][j-1]++;
+			}
+			if(j < y - 1){
+				grade[i+1][j+1]++;
+			}
+			grade[i+1][j]++;
+		}
+		if(j != y-1){
+			grade[i][j+1]++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Campo c = new Campo(5, 5, 10);
+		c.novoJogo();
+		int cont = 0;
+		for(int i = 0; i < c.x; i++){
+			for(int j = 0; j < c.y; j++){
+				if (c.grade[i][j] >= 9) {
+					cont++;
+				}
+				System.out.print("" + c.grade[i][j] + " ");
+			}
+			System.out.println("-> " + cont);
+		}
+	}
+}
